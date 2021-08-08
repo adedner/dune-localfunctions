b991e7c78c98559639c1793232ac922eed469e98
Simon Praetorius <simon.praetorius@tu-dresden.de>
2021-08-08 17:32:54 +0200

    Add set method to orders

--- dune/localfunctions/test/test-localfe.hh
+++ dune/localfunctions/test/test-localfe.hh
@@ -21,7 +21,6 @@
 #include <dune/geometry/quadraturerules.hh>
 #include <dune/geometry/referenceelements.hh>
 
-#include <dune/localfunctions/common/derivative.hh>
 #include <dune/localfunctions/common/virtualinterface.hh>
 #include <dune/localfunctions/common/virtualwrappers.hh>
 #include <dune/localfunctions/common/localfiniteelement.hh>
@@ -39,12 +38,10 @@ class ShapeFunctionAsFunction
 public:
   typedef typename FE::Traits::LocalBasisType::Traits::DomainType DomainType;
   typedef typename FE::Traits::LocalBasisType::Traits::RangeType RangeType;
-  typedef typename FE::Traits::LocalBasisType::Traits::JacobianType JacobianType;
 
   struct Traits {
     typedef typename FE::Traits::LocalBasisType::Traits::DomainType DomainType;
     typedef typename FE::Traits::LocalBasisType::Traits::RangeType RangeType;
-    typedef typename FE::Traits::LocalBasisType::Traits::JacobianType JacobianType;
   };
 
   typedef typename FE::Traits::LocalBasisType::Traits::RangeFieldType CT;
@@ -61,13 +58,6 @@ public:
     y = yy[shapeFunction_];
   }
 
-  void jacobian (const DomainType& x, JacobianType& y) const
-  {
-    std::vector<JacobianType> J;
-    fe_.localBasis().evaluateJacobian(x, J);
-    y = J[shapeFunction_];
-  }
-
 private:
   const FE& fe_;
   int shapeFunction_;
@@ -81,7 +71,6 @@ class ShapeFunctionAsCallable
   // These types are deliberately private: They are not part of a Callable API
   typedef typename FE::Traits::LocalBasisType::Traits::DomainType DomainType;
   typedef typename FE::Traits::LocalBasisType::Traits::RangeType RangeType;
-  typedef typename FE::Traits::LocalBasisType::Traits::JacobianType JacobianType;
 public:
 
   typedef typename FE::Traits::LocalBasisType::Traits::RangeFieldType CT;
@@ -95,13 +84,6 @@ public:
   {
     std::vector<RangeType> yy;
     fe_.localBasis().evaluateFunction(x, yy);
-    return yy[shapeFunction_];
-  }
-
-  JacobianType derivative (DomainType x) const
-  {
-    std::vector<JacobianType> yy;
-    fe_.localBasis().evaluateJacobian(x, yy);
     return yy[shapeFunction_];
   }
 
@@ -161,16 +143,6 @@ bool testLocalInterpolation(const FE& fe)
   return true;
 }
 
-template<class Domain, class Range>
-struct ConstantOneFunction
-{
-  using JacobianType = typename Dune::Impl::DerivativeTraits<Range(Domain)>::type;
-
-  Range operator() (const Domain&) const { return Range(1.0); }
-
-  JacobianType derivative (const Domain&) const { return JacobianType(0.0); }
-};
-
 
 // Check whether the space spanned by the shape functions
 // contains the constant functions
@@ -184,7 +156,7 @@ bool testCanRepresentConstants(const FE& fe,
   bool success = true;
 
   // Construct the constant '1' function
-  auto constantOne = ConstantOneFunction<typename LB::Traits::DomainType,RangeType>{};
+  auto constantOne = [](const typename LB::Traits::DomainType& xi) { return RangeType(1.0); };
 
   // Project the constant function onto the FE space
   std::vector<double> coefficients;
