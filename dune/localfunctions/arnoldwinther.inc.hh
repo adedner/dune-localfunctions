aae0802a0300c5c28d824813a3192a7e57043592
Porrmann, Maik <maik.porrmann@mailbox.tu-dresden.de>
2023-08-18 10:19:42 +0200

    Last fix for reuse test

--- dune/localfunctions/arnoldwinther.inc.hh
+++ dune/localfunctions/arnoldwinther.inc.hh
@@ -1,7 +1,8 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 // SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
-// SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception#ifndef DUNE_LOCALFUNCTIONS_ARNOLDWINTHER_INC_HH
+// SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
+#ifndef DUNE_LOCALFUNCTIONS_ARNOLDWINTHER_INC_HH
 #define DUNE_LOCALFUNCTIONS_ARNOLDWINTHER_INC_HH
 namespace Dune {
 namespace Impl {
