588428560fe2d4b89ce4b53e96863694b59c8aed
Alexander Schell <alexander.schell@uni-muenster.de>
2025-06-06 14:12:45 +0200

    Update internal helper structures to support SIMD interpolation
    - Updated the InterpolationHelper used by RaviartThomasSimplexInterpolation and NedelecSimplexInterpolation.
    - Inside the NedelecSimplexInterpolation and RaviartThomasSimplexInterpolation some types based on the scalar basis had to be changed to allow SIMD types.
    - Updated the DualQ1 interpolation. The system that has to be solved for the dual coefficients should remain scalar. No SIMD type should be used here.
    - Cleaned up some tests

--- dune/localfunctions/test/test-localfe.hh
+++ dune/localfunctions/test/test-localfe.hh
@@ -762,7 +762,7 @@ bool testFE(const FE& fe,
   // Order of the quadrature rule used to generate test points
   unsigned int quadOrder = 2;
 
-  typedef typename FE::Traits::LocalBasisType::Traits::RangeType::field_type CT;
+  using CT = typename FE::Traits::LocalBasisType::Traits::RangeType::field_type;
 
   bool success = true;
 
@@ -829,7 +829,6 @@ bool testFE(const FE& fe,
   if constexpr (not FEisVirtual)
     if (not (disabledTests & DisableSimdInterpolation))
     {
-      std::cout << "------- Testing SIMD interpolation -----------" << std::endl;
       success = testRandomInterpolation<FE,Dune::LoopSIMD<CT, 4>>(fe) and success;
     }
 
