2bda252a7b1ec7474b7737724bf3cb417912906e
Robert K <robertk@posteo.org>
2026-03-09 12:36:15 +0100

    [cleanup][BDFM] Added general header.

---
+++ dune/localfunctions/brezzidouglasfortinmarini.hh
@@ -0,0 +1,12 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+// SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
+// SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
+#ifndef DUNE_LOCALFUNCTIONS_BREZZIDOUGLASFORTINMARINI_HH
+#define DUNE_LOCALFUNCTIONS_BREZZIDOUGLASFORTINMARINI_HH
+
+// BDM implementations with compile-time order
+#include <dune/localfunctions/brezzidouglasfortinmarini/bdfmcube.hh>
+#include <dune/localfunctions/brezzidouglasfortinmarini/bdfmsimplex.hh>
+
+#endif // #ifndef DUNE_LOCALFUNCTIONS_BREZZIDOUGLASFORTINMARINI_HH
